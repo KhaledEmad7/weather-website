1b7fb71994da809d78446536506db8666f953932
Your Name <you@example.com>
2020-10-03 18:56:06 +0200

    Remove unnecessary log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript file is loaded!')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const message1 = document.querySelector("#message1");
